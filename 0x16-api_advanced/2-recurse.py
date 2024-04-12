a2ffce6fe97b9f3f1e6ab8a8bff69a0ae1b0c6df
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-04-12 03:43:42 +0200

    A APIS

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "CustomClient/1.0"
+        "User-Agent": ""
     }
     params = {
         "after": after,
